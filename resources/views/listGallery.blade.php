da3925bab5ecc0e13cd605f7c5f07f66498646ca
andy brookes <andybrookestar@gmail.com>
2025-02-04 16:02:46 +0000

    more twesks

--- resources/views/listGallery.blade.php
+++ resources/views/listGallery.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
 
-<blockquote class= "prophet">
-<h4 class="anticSlab">List of gallery  as follows :</h4>
-  </blockquote>
+			<blockquote class= "prophet">
+				<br><br>
+			<h4 class="anticSlab">List of gallery  as follows :</h4>
+			  
+			  </blockquote>
+<br><br>
+
 <div class ="portfolio mx-2">
-@foreach($gallery as $output)
-
-
-
- 
-  
-  
- 
-      
-      
-       <div class="delBlogImg2"> 
-		   {{ $output->Description }}
-    <div class ="item">      <img  src ="{!!url('galleryImages/'.$output->Name) !!}" class="img-fluid"  > </div>
-     </div>
+                  
+                  
+                   @foreach($gallery as $output)
+                   
+                   
+		   
+    <div class ="item">  {{ $output->Description }}    <img  src ="{!!url('galleryImages/'.$output->Name) !!}" class="img-fluid"  > </div>
+     
   <br>
-   
-  
-   
-   
     @endforeach
+  </div>
+   
+   
+   
 .
-</div>
+
 <br><br>
 {{ $gallery->links() }}
+
+
 @endsection
